a91b57f9f774d9ec90f6470b5e80ab90f0ad36aa
Joseph Christopher Jacob <36853161+X10TION@users.noreply.github.com>
2022-12-04 06:39:38 +0100

    Update main.cpp

--- c++/main.cpp
+++ c++/main.cpp
@@ -4,15 +4,15 @@ using namespace std;
 
 int main() {
 	
-	cout<<"##						   #"<< endl ;
-	cout<<"# #						  #" << endl;
-	cout<<"#  #						 #" << endl;
-	cout<<"#    #				   #" << endl;
-	cout<<"#      #				 #" << endl;
-	cout<<"#        #			#" << endl;
-	cout<<"#          #		  #" << endl;
-	cout<<"#			#	#" << endl;
-	cout<<"#			##" << endl;
+	cout<<"##			#"<< endl ;
+	cout<<"# #			#" << endl;
+	cout<<"#  #			#" << endl;
+	cout<<"#    #		      #" << endl;
+	cout<<"#      #		     #" << endl;
+	cout<<"#        #	   #" << endl;
+	cout<<"#          #      #" << endl;
+	cout<<"#	    #	#" << endl;
+	cout<<"#	      ##" << endl;
 	cout<<"#" << endl;
 	cout<<"#" << endl;
 	cout<<"#" << endl;
